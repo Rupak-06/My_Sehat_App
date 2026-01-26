d03ce39de9847f494c12785a5c3ca70575e40048
AakankshaRaghupatruni <r.aakankshasaisri@gmail.com>
2026-01-27 02:04:23 +0530

    Delete BACKEND directory

--- BACKEND/diagnostics_backend/diagnostics_app/api/deps.py
+++
@@ -1,9 +0,0 @@
-from typing import Generator
-from diagnostics_backend.diagnostics_app.db.session import SessionLocal
-
-def get_db() -> Generator:
-    try:
-        db = SessionLocal()
-        yield db
-    finally:
-        db.close()
--- BACKEND/diagnostics_backend/diagnostics_app/db/base.py
+++
@@ -1,5 +0,0 @@
-from typing import Any
-from sqlalchemy.orm import DeclarativeBase
-
-class Base(DeclarativeBase):
-    id: Any
