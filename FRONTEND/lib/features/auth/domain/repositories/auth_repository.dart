04eccec7d55c855fd2f93f95b91672b6dcd7674d
AakankshaRaghupatruni <r.aakankshasaisri@gmail.com>
2026-01-27 02:20:42 +0530

    Delete FRONTEND directory

--- FRONTEND/lib/features/auth/domain/repositories/auth_repository.dart
+++
@@ -1,9 +0,0 @@
-import '../../../../core/usecases/usecase.dart';
-import '../entities/user_entity.dart';
-
-abstract class AuthRepository {
-  Future<Result<UserEntity>> login(String phoneNumber, String otp);
-  Future<Result<UserEntity>> updateProfile(UserEntity user);
-  Future<Result<UserEntity?>> getCurrentUser();
-  Future<Result<void>> logout();
-}
